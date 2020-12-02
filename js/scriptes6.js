a3a427a8c52733de9044091e04da0ca9c157bfe1
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2020-12-02 09:22:08 +0100

    versione in ES6
    con errori

---
+++ js/scriptes6.js
@@ -0,0 +1,16 @@
+// versione ES6----------------
+$(document).ready(function() {
+
+  var numeri2 = [ ];
+  const numeriCompresi = (array, min, max) => {
+    for (i=min; i <= max; i++) {
+      numeri2.push(array[i]);
+    }
+  };
+
+  var numeri= ['teo', 'luca', 'giovanni', 'andrea', 'marco', 'federico', 'tommaso' ];
+  console.log(numeri);
+  var a = 3;
+  var b = 6;
+});
+// versione ES6----------------
